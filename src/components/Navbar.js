b31704a69205c14617177aadc16c9b71efbc85ff
Nitya Singh <nityasingh1273@gmail.com>
2021-12-02 10:24:48 +0530

    first message

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Navbar.scss";
+const Navbar = () => {
+  return (
+    <div className="navbar-wrapper">
+      <div className="navbar-brand">
+        <img src="favicon.ico" alt="brand-logo" />
+      </div>
+      {/* <NavbarLinks /> */}
+    </div>
+  );
+};
+
+export default Navbar;
