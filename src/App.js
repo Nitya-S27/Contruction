ad5dc0d206011375f7c61aef4206d94671ed8583
Jayesh Singh <jayeshsingh001@gmail.com>
2021-12-03 18:18:20 +0530

    finish-contact-us-UI-part

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Construction from "./components/Construction";
 import Footer from "./components/Footer";
+import ContactUs from "./components/ContactUs";
 
 function App() {
   return (
     <>
       <Navbar />
       <Construction />
+      <ContactUs />
       <Footer />
     </>
   );
